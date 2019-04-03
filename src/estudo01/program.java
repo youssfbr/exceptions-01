9243f789de82053cadf62ff2d2080d86e22ad755
Alisson Youssf <youssfbr@gmail.com>
2019-04-03 15:19:13 -0300

    Exception - 01

---
+++ src/estudo01/program.java
@@ -0,0 +1,29 @@
+package estudo01;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid Position");
+		}
+		catch (InputMismatchException e) {
+			System.out.println("Input error!");
+		}
+		
+		System.out.println("End of program");
+		
+		sc.close();
+	}
+
+}
